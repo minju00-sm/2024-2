ad444e57e0d8d76212e8e790570ec3a90def5134
minju00-sm <m2nzoo@sookmyung.ac.kr>
2024-09-14 18:06:54 +0900

    change

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 
 int main(int argc, char *argv[]) {
 	
+	int a, b; //정수 변수 선언
+	a = 10;
+	b = 20; 
+	
 	printf("Hello, Sookmyung Electronics!\n");
+	printf("%d + %d = %d\n", a, b, a+b);
 	return 0;
 }
